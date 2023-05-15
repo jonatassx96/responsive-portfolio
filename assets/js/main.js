85909ab41d509d88807b8b8b0c5a2bc45e0ece6b
jonatassx96 <jonatas.sx96@gmail.com>
2023-05-15 20:35:29 -0300

    feat: add all media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,102 @@
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
   });
 }
 
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
   });
 }
 
 /*========== REMOVE MENU MOBILE ==========*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
-}
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
 
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*========== ADD BLUR TO HEADER ==========*/
 const blurHeader = () => {
-  const header = document.getElementById('header')
+  const header = document.getElementById("header");
   //When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
-  this.scrollY >= 50 ? header.classList.add('blur-header')
-                     : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 /*========== EMAIL JS ==========*/
-const contactForm = document.getElementById('contact-form'),
-      contactMessage = document.getElementById('contact-message')
+const contactForm = document.getElementById("contact-form"),
+  contactMessage = document.getElementById("contact-message");
 
-const sendEmail = (e) =>{
-  e.preventDefault()
+const sendEmail = (e) => {
+  e.preventDefault();
   // serviceID - templateID - #form - publicKey
-  emailjs.sendForm('service_or0kxqk', 'template_6acrxcv', '#contact-form', '8ix5GGCUSnOiK8AHd')
-    .then(() =>{
-      //Show sent mesage
-      contactMessage.textContent = 'Message sent sucessfully ✔️'
-      
-      //Remove message after five seconds
-      setTimeout(() =>{
-        contactMessage.textContent = ''
-      }, 5000)
+  emailjs
+    .sendForm(
+      "service_or0kxqk",
+      "template_6acrxcv",
+      "#contact-form",
+      "8ix5GGCUSnOiK8AHd"
+    )
+    .then(
+      () => {
+        //Show sent mesage
+        contactMessage.textContent = "Message sent sucessfully ✔️";
 
-      // Clear input fields
-      contactForm.reset()
-    }, () =>{
-      //Show error message
-      contactMessage.textContent = 'Message not sent (service error) ❌'
+        //Remove message after five seconds
+        setTimeout(() => {
+          contactMessage.textContent = "";
+        }, 5000);
 
-    })
+        // Clear input fields
+        contactForm.reset();
+      },
+      () => {
+        //Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+      }
+    );
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+/*========== SHOW SCROLL UP ==========*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll up
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener('scroll', scrollUp)
+
+/*========== SCROLL SECTION ACTIVE LINK ==========*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+      const scrollY = window.pageYOffset
+
+      sections.forEach(current =>{
+          const sectionHeight = current.offsetHeight,
+                sectionTop = current.offsetTop -58,
+                sectionId = current.getAttribute('id'),
+                sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+          if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+          } else {
+            sectionsClass.classList.remove('active-link')
+          }
+      })
 }
-
-contactForm.addEventListener('submit', sendEmail)
+window.addEventListener('scroll', scrollActive)
